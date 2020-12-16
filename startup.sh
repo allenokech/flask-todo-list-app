6a702f4bb7196079fe7421d364f538ebbf6118d6
allen.okech <user@my-flask-vm.europe-west2-c.c.leafy-catbird-296317.internal>
2020-12-16 10:54:15 +0000

    Made changes to startup.sh

--- startup.sh
+++ startup.sh
@@ -7,5 +7,5 @@ pip3 install -r requirements.txt
 sudo mkdir /opt/flask_todo_db
 sudo chown -R jenkins /opt/flask_todo_db
 sudo systemctl daemon-reload
-sudo systemctl stop app.service
-sudo systemctl start app.service
+sudo systemctl stop flask-todo-list.service
+sudo systemctl start flask-todo-list.service
